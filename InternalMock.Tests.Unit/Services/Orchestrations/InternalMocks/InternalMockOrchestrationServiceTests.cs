ef37da582b72ba40cb155439bacadf81e9668d8f
Hassan Habib <hassanhabib@live.com>
2022-04-23 17:31:45 -0700

    ShouldMockMethods -> FAIL

---
+++ InternalMock.Tests.Unit/Services/Orchestrations/InternalMocks/InternalMockOrchestrationServiceTests.cs
@@ -0,0 +1,74 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using InternalMock.Services.Foundations.Patchings;
+using InternalMock.Services.Foundations.Reflections;
+using InternalMock.Services.Orchestrations.InternalMocks;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace InternalMock.Tests.Unit.Services.Orchestrations.InternalMocks
+{
+    public partial class InternalMockOrchestrationServiceTests
+    {
+        private readonly Mock<IReflectionService> reflectionServiceMock;
+        private readonly Mock<IPatchService> patchServiceMock;
+        private readonly IInternalMockOrchestrationService internalMockOrchestrationService;
+
+        public InternalMockOrchestrationServiceTests()
+        {
+            this.reflectionServiceMock = new Mock<IReflectionService>();
+            this.patchServiceMock = new Mock<IPatchService>();
+
+            this.internalMockOrchestrationService = new InternalMockOrchestrationService(
+                reflectionService: this.reflectionServiceMock.Object,
+                patchService: this.patchServiceMock.Object);
+        }
+
+        private static string GetRandomMethodName() =>
+           new MnemonicString().GetValue();
+
+        private static Type GetRandomType()
+        {
+            int randomNumber =
+                new IntRange(0, 3).GetValue();
+
+            return randomNumber switch
+            {
+                0 => typeof(String),
+                1 => typeof(Int64),
+                _ => typeof(Boolean)
+            };
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(2, 10).GetValue();
+
+        private static Type[] GetRandomTypes()
+        {
+            int randomNumber = GetRandomNumber();
+
+            return Enumerable.Range(start: 0, count: randomNumber)
+                .Select(item => GetRandomType()).ToArray();
+        }
+
+        private static MethodInfo CreateRandomMethodInfo()
+        {
+            string randomMethodName = GetRandomMethodName();
+            Type randomType = GetRandomType();
+            Type[] randomTypes = GetRandomTypes();
+
+            return new DynamicMethod(
+                name: randomMethodName,
+                returnType: randomType,
+                parameterTypes: randomTypes);
+        }
+    }
+}
---
+++ InternalMock/Services/Orchestrations/InternalMocks/InternalMockOrchestrationService.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using InternalMock.Services.Foundations.Patchings;
+using InternalMock.Services.Foundations.Reflections;
+
+namespace InternalMock.Services.Orchestrations.InternalMocks
+{
+    public class InternalMockOrchestrationService : IInternalMockOrchestrationService
+    {
+        private readonly IReflectionService reflectionService;
+        private readonly IPatchService patchService;
+
+        public InternalMockOrchestrationService(
+            IReflectionService reflectionService,
+            IPatchService patchService)
+        {
+            this.reflectionService = reflectionService;
+            this.patchService = patchService;
+        }
+
+        public void Mock(string internalMethodName, Type type, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ThrowException(Exception exception) => throw exception;
+    } 
+}
