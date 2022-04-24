242dda22f8d3b009553efe354b2c5d8a26d26d81
Hassan Habib <hassanhabib@live.com>
2022-04-24 01:50:17 -0700

    EXPOSER: Change API to Match Moq

--- InternalMock/Extensions/InternalMockExtensions.cs
+++ InternalMock/Extensions/InternalMockExtensions.cs
@@ -7,6 +7,7 @@
 using System;
 using InternalMock.Brokers.Patchings;
 using InternalMock.Brokers.Reflections;
+using InternalMock.Models.Extensions;
 using InternalMock.Services.Foundations.Patchings;
 using InternalMock.Services.Foundations.Reflections;
 using InternalMock.Services.Orchestrations.InternalMocks;
@@ -17,13 +18,22 @@ namespace InternalMock.Extensions
     {
         private static IInternalMockOrchestrationService InternalMockOrchestrationService;
 
-        public static void Mock(this object service, string methodName, Exception exception)
+        public static MockedMethod Mock(this object service, string methodName)
+        {
+            return new MockedMethod
+            {
+                Type = service.GetType(),
+                MethodName = methodName
+            };
+        }
+
+        public static void Throws(this MockedMethod mockedMethod, Exception exception)
         {
             InternalMockOrchestrationService = CreateMockService();
 
             InternalMockOrchestrationService.Mock(
-                methodName,
-                service.GetType(),
+                mockedMethod.MethodName,
+                mockedMethod.Type,
                 exception);
         }
 
---
+++ InternalMock/Models/Extensions/MockedMethod.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace InternalMock.Models.Extensions
+{
+    public class MockedMethod
+    {
+        public Type Type { get; set; }
+        public string MethodName { get; set; }
+    }
+}
